7151d117d53e5f601d236671add68c0ff71400f6
Hack Reactor Student <student@hackreactor.com>
2017-04-25 11:26:27 -0700

    Create hashPassword function

--- server/models/user.js
+++ server/models/user.js
@@ -1,46 +1,34 @@
-const utils = require('../lib/hashUtils');
+const crypto = require('crypto');
+const Promise = require('bluebird');
+const request = Promise.promisify(require('request'), { multiArgs: true });
+const utils = require('../lib/hashUtils.js');
 const Model = require('./model');
-// crypto
 
 class Users extends Model {
   constructor() {
     super('users');
   }
-
-  // createUserName(user, cb) {
-  //   var dbString = `INSERT INTO users (username) VALUES ("${user}")`;
-  //   // db.query(dbString, cb);
-  //   create(options) {
-  //     let queryString = `INSERT INTO ${this.tablename} SET ?`;
-  //     return executeQuery(queryString, options);
-  //   }
-  // }
-
-  // createUserPassword(password, cb) {
-  //   var dbString = `INSERT INTO users (password) VALUES ("${password}")`;
-  //   db.query(dbString, cb);
-  // }
+  // crypto to hash password
+  hashPassword(password) {
+    var hashedPW = utils.makeHash(password);
+    return hashedPW;
+  }
 }
 
 module.exports = new Users();
 
-// class Links extends Model {
 
-// Model.create(){}
 
-// create(options) {
-//   let queryString = `INSERT INTO ${this.tablename} SET ?`;
-//   return executeQuery(queryString, options);
+// createUserName(user, cb) {
+//   var dbString = `INSERT INTO users (username) VALUES ("${user}")`;
+//   // db.query(dbString, cb);
+//   create(options) {
+//     let queryString = `INSERT INTO ${this.tablename} SET ?`;
+//     return executeQuery(queryString, options);
+//   }
 // }
 
-// get: function(){},
-// post: function (options, cb) {
-//   console.log('inside user post!')
-//   var user = options.json.username;
-//   var password = options.json.password;
-//   console.log('USER: ', user, 'PASSWORD: ', password);
-//   var dbString = 'INSERT INTO users (username, password) VALUES (?, ?)';
-//   db.query(dbString, function(err, results) {
-//     cb(err, results);
-//   });
+// createUserPassword(password, cb) {
+//   var dbString = `INSERT INTO users (password) VALUES ("${password}")`;
+//   db.query(dbString, cb);
 // }
