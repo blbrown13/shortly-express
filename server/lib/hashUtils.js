9a0359926d9764aa22a6618efa4dca0a8cdf12af
Hack Reactor Student <student@hackreactor.com>
2017-04-25 11:25:53 -0700

    Add makeHash function using crypto to hash password

--- server/lib/hashUtils.js
+++ server/lib/hashUtils.js
@@ -4,3 +4,10 @@ const crypto = require('crypto');
 // Add any hashing utility functions below
 /************************************************************/
 
+module.exports.makeHash = function(password) {
+  const secret = password;
+  const hash = crypto.createHmac('sha256', secret)
+                      .update('I love cupcakes')
+                      .digest('hex');
+  return hash;
+}
