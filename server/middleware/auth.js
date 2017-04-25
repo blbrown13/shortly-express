e566bc3aa374571f0a350b8169b5cf7da80d6587
Hack Reactor Student <student@hackreactor.com>
2017-04-24 20:09:33 -0700

    Refactor code to pass first 5 specs

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,10 +1,15 @@
 const models = require('../models');
 const Promise = require('bluebird');
 
+
 module.exports.createSession = (req, res, next) => {
+  // something with request and response
+  // app.post('/', function (req, res) {
+  //   res.send('POST request to the homepage')
+  // });
+  // next();
 };
 
 /************************************************************/
 // Add additional authentication middleware functions below
 /************************************************************/
-
