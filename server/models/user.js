6f42d52df180c032a6049577fc25a6d63dcfae18
Hack Reactor Student <student@hackreactor.com>
2017-04-24 20:07:32 -0700

    Refactor class Users to extend Model and add create user function

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,48 @@
+const db = require('../db/index.js');
 const utils = require('../lib/hashUtils');
 const Model = require('./model');
 
 // Write you user database model methods here
+// module.exports = {
+//   createUser: {
+//
+//   }
+// };
 
+class Users extends Model {
+  constructor() {
+    super('users');
+  }
+
+  create(options) {
+    db.query(options, function(err, results) {
+      cb(err, results);
+    });
+  }
+}
+
+module.exports = new Users();
+
+// class Links extends Model {
+
+
+// Model.create(){}
+
+
+// create(options) {
+//   let queryString = `INSERT INTO ${this.tablename} SET ?`;
+//   return executeQuery(queryString, options);
+// }
+
+
+// get: function(){},
+// post: function (options, cb) {
+//   console.log('inside user post!')
+//   var user = options.json.username;
+//   var password = options.json.password;
+//   console.log('USER: ', user, 'PASSWORD: ', password);
+//   var dbString = 'INSERT INTO users (username, password) VALUES (?, ?)';
+//   db.query(dbString, function(err, results) {
+//     cb(err, results);
+//   });
+// }
